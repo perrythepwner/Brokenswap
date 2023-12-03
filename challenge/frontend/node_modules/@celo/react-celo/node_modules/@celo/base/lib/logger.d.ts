8c5bb1552ba6929981014a24d5743e685a5b13bf
perrythepwner <perrythepwner@gmail.com>
2023-12-03 21:22:48 +0100

    fixed set command

--- challenge/frontend/node_modules/@celo/react-celo/node_modules/@celo/base/lib/logger.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type Logger = (...args: any[]) => void;
-/** @internal */
-export declare const noopLogger: Logger;
-/** @internal */
-export declare const prefixLogger: (prefix: string, logger: Logger) => Logger;
-/** @internal */
-export declare const consoleLogger: Logger;
--- challenge/frontend/node_modules/@celo/wallet-local/node_modules/@celo/connect/lib/utils/provider-utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { Provider } from '../types';
-/** @internal */
-export declare function hasProperty<T>(object: any, property: string): object is T;
-/** @internal */
-export declare function stopProvider(defaultProvider: Provider): void;
